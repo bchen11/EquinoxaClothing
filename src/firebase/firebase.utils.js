874fba901b0997b13f88442b0f8ca91cd0fb2c4e
binlinchen <42919196+bchen11@users.noreply.github.com>
2022-01-24 16:55:25 -0500

    store users into firebase cloud

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,21 +3,44 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
 const config = {
-    apiKey: "AIzaSyC_xC77bLPwYbIpp6_RxpydlYTcW4oC0aM",
-    authDomain: "ecomdb-b4844.firebaseapp.com",
-    projectId: "ecomdb-b4844",
-    storageBucket: "ecomdb-b4844.appspot.com",
-    messagingSenderId: "2900269689",
-    appId: "1:2900269689:web:9d7ee1e75e9af338de92a8",
-    measurementId: "G-XLQF9HFT7S"
-  };
+  apiKey: "AIzaSyC_xC77bLPwYbIpp6_RxpydlYTcW4oC0aM",
+  authDomain: "ecomdb-b4844.firebaseapp.com",
+  projectId: "ecomdb-b4844",
+  storageBucket: "ecomdb-b4844.appspot.com",
+  messagingSenderId: "2900269689",
+  appId: "1:2900269689:web:9d7ee1e75e9af338de92a8",
+  measurementId: "G-XLQF9HFT7S"
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
-
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
